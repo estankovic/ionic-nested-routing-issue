5ca6eebcbc1bebd1fe4b25ae1fa301908379f64e
Eduard Stankovic <eduard.stankovic.ext@uniqa.at>
2021-01-22 16:35:40 +0100

    Fixed issue with Components not being part of any module

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,5 +1,4 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -7,6 +6,8 @@ import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import {SubTab2Component} from './sub-tab2/sub-tab2.component';
+import {SubTab1Component} from './sub-tab1/sub-tab1.component';
 
 @NgModule({
   imports: [
@@ -14,8 +15,12 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
   ],
-  declarations: [Tab2Page]
+  declarations: [
+    Tab2Page,
+    SubTab1Component,
+    SubTab2Component,
+  ]
 })
 export class Tab2PageModule {}
